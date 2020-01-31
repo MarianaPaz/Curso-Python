f3079141d8409cae6398f7533d7d4c6adf30ce03
MarianaPaz <m.paz1971@hotmail.com>
2020-01-30 20:37:51 -0500

    Add files via upload

---
+++ fibo.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 18 12:07:57 2020
+
+@author: CEC
+"""
+import fibonaci 
+fibonaci.fib(100)
+
---
+++ fibonaci.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 18 12:22:05 2020
+
+@author: CEC
+"""
+
+def fib(n):
+    a, b = 0,1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+fib(20)
